5f1c3650c5e1ca43e1529bbbd9014be1c2e1f7b2
Alex Mercadel <alexmercadel@Alexs-MacBook-Pro.local>
2025-03-06 10:16:55 -0600

    fixed file path issue

--- src/findFalseIBDs.sh
+++ src/findFalseIBDs.sh
@@ -4,5 +4,5 @@ gt_segments_input=$2
 cm_cutoff=$3
 false_ibd_cutoff=$4
 
-python findFalseIBD.py $hap_input $gt_segments_input $cm_cutoff $false_ibd_cutoff
-python calculateStatistics.py $hap_input $gt_segments_input $cm_cutoff
+python src/findFalseIBD.py $hap_input $gt_segments_input $cm_cutoff $false_ibd_cutoff
+python src/calculateStatistics.py $hap_input $gt_segments_input $cm_cutoff
